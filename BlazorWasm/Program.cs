138c507c611f832b5d842dcfda61822d09985f2d
Sangbong Park <sangbong.park@outlook.com>
2021-06-27 08:34:09 -0400

    Update Pratice

--- BlazorWasm/Program.cs
+++ BlazorWasm/Program.cs
@@ -7,7 +7,7 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
-
+using MudBlazor.Services;
 namespace BlazorWasm
 {
     public class Program
@@ -18,7 +18,7 @@ namespace BlazorWasm
             builder.RootComponents.Add<App>("#app");
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-
+            builder.Services.AddMudServices();
             await builder.Build().RunAsync();
         }
     }
